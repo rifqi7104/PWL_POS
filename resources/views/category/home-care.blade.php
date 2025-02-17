353fff9c85fc7166fda962fe7d45b7090925315a
Muhammad Rifqi Rizqullah <rifqirizqullah7104@gmail.com>
2025-02-17 16:42:19 +0700

    Jobsheet 2 Soal Praktikum

---
+++ resources/views/category/home-care.blade.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Category Home Care | POS</title>
+</head>
+<body>
+    <h1>Category : Home Care</h1>
+    <ul>
+        <li>Sapu</li>
+        <li>Vacuum Cleaner</li>
+        <li>Sabun</li>
+        <li>Spons</li>
+    </ul>    
+</body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\SalesController;
+use App\Http\Controllers\UserController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "web" middleware group. Make something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('home');
+});
+
+Route::get('/user/{id}/name/{name}', [UserController::class, 'user']);
+
+Route::prefix('category')->group(function () {
+    Route::get('/food-beverage', [CategoryController::class, 'foodBeverage']);
+    Route::get('/beauty-health', [CategoryController::class, 'beautyHealth']);
+    Route::get('/home-care', [CategoryController::class, 'homeCare']);
+    Route::get('/baby-kid', [CategoryController::class, 'babyKid']);
+    });
+
+Route::get('/sales', [SalesController::class, 'sales']);
+
