9ba3517a8b4cede6a6f544339b3017d89c94c3a5
Muhammad Rifqi Rizqullah <rifqirizqullah7104@gmail.com>
2025-03-16 21:29:57 +0700

    Js6 2 - Modal Ajax Edit Data (Data User)

--- resources/views/user/index.blade.php
+++ resources/views/user/index.blade.php
@@ -61,7 +61,7 @@
 
         var dataUser;
         $(document).ready(function() {
-            var dataUser = $('#table_user').DataTable({
+                dataUser = $('#table_user').DataTable({
                 // serverSide: true, jika ingin menggunakan server side processing
                 processing: true,
                 serverSide: true,
