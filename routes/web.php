540081e3b005c6e76cfcb31e1cdf03cf5d146e43
Muhammad Rifqi Rizqullah <rifqirizqullah7104@gmail.com>
2025-04-12 15:29:28 +0700

    Js8 2 - Export Data ke Excel

--- routes/web.php
+++ routes/web.php
@@ -60,6 +60,7 @@ Route::middleware(['auth'])->group(function () {
             Route::post('/ajax', [BarangController::class, 'store_ajax']);         // Menyimpan data user baru Ajax
             Route::get('/import', [BarangController::class, 'import']); //ajax form upload excel
             Route::post('/import_ajax', [BarangController::class, 'import_ajax']); // ajax import excel
+            Route::get('/export_excel', [BarangController::class, 'export_excel']); // ajax import excel
             Route::get('/{id}', [BarangController::class, 'show']);
             Route::get('/{id}/edit', [BarangController::class, 'edit']);
             Route::put('/{id}', [BarangController::class, 'update']);
